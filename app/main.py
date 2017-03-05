c172ec4992fbda529738f2dec8e9452470344f68
Richard M <puzzleprogrammer@gmail.com>
2017-03-04 18:51:55 -0800

    okay

--- app/main.py
+++ app/main.py
@@ -54,6 +54,8 @@ def move():
 		dir = ['down','left','right']
 	elif head[1] < last[1]:
 		dir = ['up','left','right']
+	if head[0] == 0:
+		pass
 	left = head.copy()
 	right=head.copy()
 	up=head.copy()
@@ -62,38 +64,9 @@ def move():
 	right[0]+=1
 	up[1]-=1
 	down[1]+=1
-	for snake in data["snakes"]:
-		if up in snake["coords"]:
-			up = [-1,-1]
-		if down in snake["coords"]:
-			down = [-1,-1]
-		if left in snake["coords"]:
-			left = [-1,-1]
-		if right in snake["coords"]:
-			right = [-1,-1]
-	if right[0] == data['height']:
-		right == [-1,-1]
-	if down[1] == data['height']:
-		down == [-1,-1]
-	mv =[]
-	if not -1 in down and 'down' in dir:
-		mv += ['down']
-	
-	if not -1 in right and 'right' in dir:
-		mv += ['right']
-	
-	if not -1 in left and 'left' in dir:
-		mv += ['left']
-	
-	if not -1 in up and 'up' in dir:
-		mv += ['up']
-	
-	r=random.randint(0,len(mv))
-	if len(mv)==0:
-		r=0
-		mv=['right']
+	r=random.randint(0,len(dir))
 	return {
-		'move': 'left',
+		'move': dir[r],
 		'taunt': "AHHH!"
 	}
 
